607c80bf7992bbce0fd5fe316c09a0136e6ae00d
Phuc Anh <tranphucanh11@gmail.com>
2022-11-08 10:29:38 +0700

    update code

---
+++ src/main/java/moirottoiec/salesmanagement/BLL/CustomerBLL.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package moirottoiec.salesmanagement.BLL;
+
+import java.util.Iterator;
+import java.util.List;
+import moirottoiec.salesmanagement.DAL.CustomerDAL;
+import moirottoiec.salesmanagement.DAL.testDAL;
+import moirottoiec.salesmanagement.Entity.Category;
+import moirottoiec.salesmanagement.Entity.Customer;
+
+/**
+ *
+ * @author anhph
+ */
+public class CustomerBLL {
+    private CustomerDAL customerDAL;
+    public List getListCustomer(){
+        customerDAL = new CustomerDAL();
+            
+            //getList
+        List results = customerDAL.getListCustomer();
+        
+        //close Session
+        customerDAL.close();
+        return results;
+    }
+    
+    public void addCustomer(Customer customer){
+        customerDAL.addCustomer(customer);
+    }
+    
+    public void updateCustomer(Customer customer){
+        customerDAL.updateCustomer(customer);
+    }
+    
+    public void deleteCustomer(Customer customer){
+        customerDAL.deleteCustomer(customer);
+    }
+}
---
+++ src/main/java/moirottoiec/salesmanagement/DAL/CustomerDAL.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package moirottoiec.salesmanagement.DAL;
+
+import java.util.List;
+import moirottoiec.salesmanagement.Entity.Category;
+import moirottoiec.salesmanagement.Entity.Customer;
+
+/**
+ *
+ * @author anhph
+ */
+public class CustomerDAL extends ManagerDAL{
+    public CustomerDAL(){
+        super();
+    }
+    
+    public List getListCustomer(){
+        List results=null;
+        try {
+            beginTransaction();
+            String hql = " FROM Customer c";
+            results = getSession().createQuery(hql,Customer.class).list();
+            getTransaction().commit();
+        } catch (Exception e) {
+           if(getTransaction()!=null && getTransaction().isActive()){
+                getTransaction().rollback();
+            }
+        } 
+        return results;
+    }
+    
+    public void addCustomer(Customer customer){
+        try {
+            beginTransaction();
+            getSession().save(customer);
+            getTransaction().commit();
+            getSession().flush();
+        } 
+        catch (Exception e) {
+            if(getTransaction()!=null && getTransaction().isActive()){
+                getTransaction().rollback();
+            }
+        }
+    }
+    
+    public void updateCustomer(Customer customer){
+        try{
+            beginTransaction();
+            getSession().update(customer);
+            getTransaction().commit();
+            getSession().flush();
+        }catch(Exception e){
+            if(getTransaction()!=null && getTransaction().isActive()){
+                getTransaction().rollback();
+            }
+        }
+    }
+    
+    public void deleteCustomer(Customer customer){
+        try {
+            beginTransaction();
+            getSession().delete(customer);
+            getTransaction().commit();
+            getSession().flush();
+        } 
+        catch (Exception e) {
+            if(getTransaction()!=null && getTransaction().isActive()){
+                getTransaction().rollback();
+            }
+        }
+    }
+}
